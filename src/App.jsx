b1c4362d914bc6a3da2a423f181cca05520dbf1b
SunnyKapur <kumarsunnykumar678@gmail.com>
2025-09-17 20:26:14 +0530

    two way binding form elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,58 @@
-import { useState } from "react";
-import Create from "./components/Create";
-import Read from "./components/Read";
+import React, { useState } from "react";
 
 const App = () => {
-  const [users, setusers] = useState([
-    { name: "John", age: 12 },
-    { name: "ankur", age: 32 },
-    { name: "sunny", age: 22 },
+  const [todos, settodos] = useState([
+    { id: 1, title: "Kaam kar le bhai", isCompleted: false },
   ]);
 
+  const [title, settitle] = useState("");
+  const [completed, setcompleted] = useState(true);
+  const [gender, setgender] = useState("male");
+  const [city, setcity] = useState("mumbai")
   return (
     <div>
-      <Create />
-      <Read users={users} setusers={setusers}/>
+      <h1>Create Tasks</h1>
+      <form>
+        <input
+          onChange={(e) => settitle(e.target.value)}
+          value={title}
+          type="text"
+          placeholder="title"
+        />
+        <br />
+        <br />
+        <input
+          checked={completed}
+          type="checkbox"
+          onChange={(e) => setcompleted(e.target.checked)}
+        />
+        Completed
+        <br />
+        <br />
+        <input
+        checked={gender == "male" && true}
+          value="male"
+          onChange={(e) => setgender(e.target.value)}
+          type="radio"
+        />
+        male
+        <input
+          checked={gender == "female" && true}
+          value="female"
+          onChange={(e) => setgender(e.target.value)}
+          type="radio"
+        />
+        Female
+        <br />
+        <br />
+        <select value={city} onChange={(e) => setcity(e.target.value)}>
+          <option value="delhi">Delhi</option>
+          <option value="mumbai">Mumbai</option>
+          <option value="kolkata">Kolkata</option>
+        </select>
+        <br /><br />
+        <button>Create Todo</button>
+      </form>
     </div>
   );
 };
